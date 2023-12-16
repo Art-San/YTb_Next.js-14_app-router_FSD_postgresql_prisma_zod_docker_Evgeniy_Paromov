2249bfc1c4cbf51217f6310c70d4a798b079a9c5
Art San <artsan.mcc@gmail.com>
2023-12-16 17:03:59 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from '@/shared/ui/button'
-import Image from 'next/image'
-
-export default function Home() {
+import { CoursesList } from '@/features/courses-list/pub/courses-list'
+import { CreateCourseForm } from '@/features/courses-list/pub/create-course-form'
+// 2:04:02
+export default async function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Button variant={'destructive'}>Button</Button>
+		<main className="flex min-h-screen flex-col  p-8">
+			<CreateCourseForm
+				revalidatePagePath="/"
+				className=" max-w-[300px] mb-5"
+			/>
+			<CoursesList revalidatePagePath="/" />
 		</main>
 	)
 }
