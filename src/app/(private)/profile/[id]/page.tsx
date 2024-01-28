019d8e16fe4defe0956c8b0be45a777d6e694d7a
Art San <artsan.mcc@gmail.com>
2024-01-28 18:17:54 +0700

    ok

--- src/app/(private)/profile/[id]/page.tsx
+++ src/app/(private)/profile/[id]/page.tsx
@@ -1,7 +1,17 @@
-import { FC } from 'react'
-
-const ProfilePageId: FC = () => {
-	return <div>ProfilePageId</div>
+// import { UpdateProfileForm } from "@/features/update-profile/update-profile-form";
+import { Separator } from '@/shared/ui/separator'
+// 3:19:31
+export default function ProfilePage({ params }: { params: { id: string } }) {
+	return (
+		<main className="space-y-6 py-14 container  max-w-[600px]">
+			<div>
+				<h3 className="text-lg font-medium">Профиль</h3>
+				<p className="text-sm text-muted-foreground">
+					Это как другие пользователи видят вас на сайте
+				</p>
+			</div>
+			<Separator />
+			{/* <UpdateProfileForm userId={params.id} /> */}
+		</main>
+	)
 }
-
-export default ProfilePageId
