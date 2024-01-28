1870968364f1e9594df0a088570ec9330fe053e4
Art San <artsan.mcc@gmail.com>
2024-01-28 22:13:30 +0700

    ok

--- src/app/(private)/profile/[id]/page.tsx
+++ src/app/(private)/profile/[id]/page.tsx
@@ -1,4 +1,4 @@
-// import { UpdateProfileForm } from "@/features/update-profile/update-profile-form";
+import { UpdateProfileForm } from '@/features/update-profile/update-profile-form'
 import { Separator } from '@/shared/ui/separator'
 // 3:19:31
 export default function ProfilePage({ params }: { params: { id: string } }) {
@@ -10,8 +10,27 @@ export default function ProfilePage({ params }: { params: { id: string } }) {
 					Это как другие пользователи видят вас на сайте
 				</p>
 			</div>
+			<h1>Профиль</h1>
 			<Separator />
-			{/* <UpdateProfileForm userId={params.id} /> */}
+
+			<UpdateProfileForm userId={params.id} />
 		</main>
 	)
 }
+// // import { UpdateProfileForm } from "@/features/update-profile/update-profile-form";
+// import { Separator } from '@/shared/ui/separator'
+// // 3:19:31
+// export default function ProfilePage({ params }: { params: { id: string } }) {
+// 	return (
+// 		<main className="space-y-6 py-14 container  max-w-[600px]">
+// 			<div>
+// 				<h3 className="text-lg font-medium">Профиль</h3>
+// 				<p className="text-sm text-muted-foreground">
+// 					Это как другие пользователи видят вас на сайте
+// 				</p>
+// 			</div>
+// 			<Separator />
+// 			{/* <UpdateProfileForm userId={params.id} /> */}
+// 		</main>
+// 	)
+// }
