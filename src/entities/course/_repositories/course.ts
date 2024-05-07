ee62804e2293dc7b85a23147ef0d590fc284a274
Art San <artsan.mcc@gmail.com>
2024-05-08 00:30:00 +0700

    ok

--- src/entities/course/_repositories/course.ts
+++ src/entities/course/_repositories/course.ts
@@ -1,44 +1,70 @@
+// import { cache } from 'react'
+// import { CourseEntity } from '../_domain/types'
+
+// import { contentApi } from '@/shared/api/content'
+
+// class CoursesRepository {
+// 	getCoursesList = cache(async (): Promise<CourseEntity[]> => {
+// 		const manifest = await contentApi.fetchManifest()
+
+// 		console.log(0, 'manifest', manifest)
+// 		return [
+// 			{
+// 				id: '1234',
+// 				slug: 'tup',
+// 				description: 'description',
+// 				title: 'title',
+// 			},
+// 		]
+// 	})
+// }
+
+// export const coursesRepository = new CoursesRepository()
+
 import { cache } from 'react'
 import { CourseEntity } from '../_domain/types'
-
-import { fetchManifest } from '@/shared/api/content'
-
+import { contentApi } from '@/shared/api/content'
+// import { logger } from "@/shared/lib/logger";
 class CoursesRepository {
 	getCoursesList = cache(async (): Promise<CourseEntity[]> => {
-		const manifest = await fetchManifest()
+		const manifest = await contentApi.fetchManifest()
+		// console.log(1, 'manifest,', manifest)
 
-		console.log(0, 'manifest', manifest)
-		return [
-			{
-				id: '1234',
-				slug: 'tup',
-				description: 'description',
-				title: 'title',
-			},
-		]
+		const fetchCourse = async (courseSlug: string): Promise<CourseEntity> => {
+			const course = await contentApi.fetchCourse(courseSlug)
+			return {
+				id: course.id,
+				title: course.title,
+				description: course.description,
+				slug: courseSlug,
+			}
+		}
+
+		const settledCourses = await Promise.allSettled(
+			manifest.courses.map(fetchCourse)
+		)
+
+		// console.log(2, settledCourses)
+
+		settledCourses.forEach((value, i) => {
+			if (value.status === 'rejected') {
+				// logger.error({
+				//   msg: "Course by slug not found",
+				//   slug: manifest.courses[i],
+				//   erorr: value.reason,
+				// });
+			}
+		})
+
+		return settledCourses
+			.filter(
+				(courseResult): courseResult is PromiseFulfilledResult<CourseEntity> =>
+					courseResult.status === 'fulfilled'
+			)
+			.map((course) => {
+				return course.value
+			})
 	})
 }
 
 export const coursesRepository = new CoursesRepository()
-// import { dbClient } from '@/shared/lib/db'
-// import { cache } from 'react'
-// class CoursesRepository {
-// 	getCoursesList = cache(
-// 		(): Promise<CourseListElement[]> => dbClient.course.findMany()
-// 	)
-
-// 	createCourseElement = (
-// 		command: CreateCourseListElementCommand
-// 	): Promise<CourseListElement> => {
-// 		return dbClient.course.create({
-// 			data: command,
-// 		})
-// 	}
-// 	deleteCourseElement = (command: DeleteCourseListElementCommand) => {
-// 		return dbClient.course.delete({
-// 			where: { id: command.id },
-// 		})
-// 	}
-// }
-
-// export const coursesRepository = new CoursesRepository()
--- src/shared/api/content/index.ts
+++ src/shared/api/content/index.ts
@@ -1,111 +1,34 @@
 import { privateConfig } from '@/shared/config/private'
 import { FileFetcher } from './_lib/file-fetcher'
 import { ContentParser } from './_lib/content-parser'
-import manifestSchema from './_schemas/manifest.schema.json'
-import { Manifest } from './_schemas/manifest.schema'
+import {
+	DummyCacheStrategy,
+	ReactQueryCacheStrategy,
+} from './_lib/cache-strategy'
+import { ContentApi } from './_content-api'
 // 1Ж24
 const fileFetcher = new FileFetcher(privateConfig.CONTENT_TOKEN)
 
 const contentParser = new ContentParser()
+const reactQueryCacheStrategy = new ReactQueryCacheStrategy()
+const dummyCacheStrategy = new DummyCacheStrategy()
 
-export const fetchManifest = async () => {
-	const text = await fileFetcher.fetchText(
-		`${privateConfig.CONTENT_URL}/manifest.yaml`
-	)
-	const manifest = await contentParser.parse<Manifest>(text, manifestSchema)
-	return manifest
-}
-// import { join } from "path";
-// import { CacheStategy } from "./_lib/cache-strategy";
-// import { ContentParser } from "./_lib/content-parser";
-// import { FileFetcher } from "./_lib/file-fetcher";
-// import manifestSchema from "./_schemas/manifest.schema.json";
-// import courseSchema from "./_schemas/course.schema.json";
-// import lessonSchema from "./_schemas/lesson.schema.json";
-// import { Course } from "./_schemas/course.schema";
-// import { Lesson } from "./_schemas/lesson.schema";
-// import { Manifest } from "./_schemas/manifest.schema";
-// import { loggedMethod } from "@/shared/lib/logger";
-// import { pick } from "lodash-es";
-
-// interface Deps {
-//   cacheStrategy: CacheStategy;
-//   contentParser: ContentParser;
-//   fileFetcher: FileFetcher;
+// export const fetchManifest = async () => {
+// 	const fetchData = async () => {
+// 		console.log('fetch data')
+// 		const text = await fileFetcher.fetchText(
+// 			`${privateConfig.CONTENT_URL}/manifest.yaml`
+// 		)
+// 		return await contentParser.parse<Manifest>(text, manifestSchema)
+// 	}
+// 	return dummyCacheStrategy.fetch(['manifest'], fetchData)
 // }
 
-// type CourseSlug = string;
-// type LessonSlug = string;
-
-// export class ContentApi {
-//   constructor(
-//     private baseUrl: string,
-//     private d: Deps,
-//   ) {}
-
-//   async fetchManifest() {
-//     return this.d.cacheStrategy.fetch(["manifest"], () =>
-//       this.fetchManifestQuery(),
-//     );
-//   }
-
-//   @loggedMethod({
-//     logRes: (res: Manifest) => res,
-//   })
-//   private async fetchManifestQuery() {
-//     const text = await this.d.fileFetcher.fetchText(this.getManifestUrl());
-//     return await this.d.contentParser.parse<Manifest>(text, manifestSchema);
-//   }
-
-//   async fetchCourse(slug: CourseSlug) {
-//     return this.d.cacheStrategy.fetch(["course", slug], () =>
-//       this.fetchCourseQuery(slug),
-//     );
-//   }
-
-//   @loggedMethod({
-//     logArgs: (slug: CourseSlug) => ({ slug }),
-//     logRes: (res: Course, slug) =>
-//       pick({ ...res, slug }, ["id", "title", "slug"]),
-//   })
-//   private async fetchCourseQuery(slug: string) {
-//     const text = await this.d.fileFetcher.fetchText(this.getCourseUrl(slug));
-//     return await this.d.contentParser.parse<Course>(text, courseSchema);
-//   }
-
-//   async fetchLesson(courseSlug: CourseSlug, lessonSlug: LessonSlug) {
-//     return this.d.cacheStrategy.fetch(["lesson", courseSlug, lessonSlug], () =>
-//       this.fetchLessonQuery(courseSlug, lessonSlug),
-//     );
-//   }
-
-//   @loggedMethod({
-//     logArgs: (courseSlug: CourseSlug, lessonSlug: LessonSlug) => ({
-//       courseSlug,
-//       lessonSlug,
-//     }),
-//     logRes: (res: Lesson) => pick(res, ["id", "title", "slug"]),
-//   })
-//   private async fetchLessonQuery(
-//     courseSlug: CourseSlug,
-//     lessonSlug: LessonSlug,
-//   ) {
-//     const text = await this.d.fileFetcher.fetchText(
-//       this.getLessonUrl(courseSlug, lessonSlug),
-//     );
-//     return await this.d.contentParser.parse<Lesson>(text, lessonSchema);
-//   }
-
-//   private getManifestUrl() {
-//     return join(this.baseUrl, "manifest.yaml");
-//   }
-//   private getCourseUrl(slug: CourseSlug) {
-//     return join(this.baseUrl, `/courses/${slug}/course.yaml`);
-//   }
-//   private getLessonUrl(courseSlug: CourseSlug, lessonSlug: LessonSlug) {
-//     return join(
-//       this.baseUrl,
-//       `/courses/${courseSlug}/lesson/${lessonSlug}/lesson.yaml`,
-//     );
-//   }
-// }
+export const contentApi = new ContentApi(privateConfig.CONTENT_URL, {
+	cacheStrategy:
+		process.env.NODE_ENV === 'development'
+			? dummyCacheStrategy
+			: reactQueryCacheStrategy,
+	contentParser,
+	fileFetcher,
+})
